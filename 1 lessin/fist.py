d589f9ee934b79cd2c1d62b0fd173fe5bd369b48
Maksim-Maley <124808155+Maksim-Maley@users.noreply.github.com>
2023-02-08 20:07:52 +0300

    first commit
    1 com

---
+++ 1 lessin/fist.py
@@ -0,0 +1,28 @@
+print("Hello WorLd!")
+
+box = 13
+print(box)
+
+a = input()
+print(a)
+
+int()
+float()
+
+a = 5
+b = int(input())
+print(a+b)
+
+a = 5
+b = 10 
+print(a>b)
+
+a = True #истина
+b = False #ложь
+
+"1234567889" #str
+[1,2,3,4,5] #list
+{1,2,3,4,5} #tuple
+
+
+
---
+++ 2 lesson/fist.py
@@ -0,0 +1,45 @@
+print(1+2)
+#операторы сравнения
+print(1==2)
+print(1>2)
+print(1<2)
+print(1!=2)
+print(1>=2)
+print(1<=2)
+
+print(1+1 == 2 or 1*1 == 2)
+print(1+1 == 1 or 1*1 == 1)
+print(1+1 == 1 or 1*1 == 2)
+print(1+1 == 2 and 1*1 == 2)
+print(1+1 == 2 and 1*1 == 1)
+print(1+1 == 2 and not 1*1 == 2)
+print(1+1 == 2 and not 1*1 == 1)
+print(1+1 == 1 or not 1*1 == 2)
+
+print(15//4)
+print(451//17)
+print(5**3)
+print(8**4)
+print(1%4)
+print(673%21)
+
+a = 13
+a += 5
+print(a)
+a -= 7
+print(a)
+a *= 4
+print(a)
+a /= 2
+print(a)
+a //= 3
+print(a)
+a **= 2
+print(a)
+a %= 15
+print(a)
+
+print("fresh" in "lemon=fresh")
+print("fresh" in "lemon13235fgsdfbfresh")
+print("fresh" in "lemon13235fgsdf")
+print("fresh" not in "lemon13235fgsdf")
