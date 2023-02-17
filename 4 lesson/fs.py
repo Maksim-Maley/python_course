1c120450af6c923d3e1387307777118743b14693
Maksim-Maley <124808155+Maksim-Maley@users.noreply.github.com>
2023-02-17 14:20:20 +0300

    4 lesson

---
+++ 4 lesson/fs.py
@@ -0,0 +1,26 @@
+print('Задание 1')
+a=0
+b=100
+c=0
+while a < b:
+    a += 1
+    c += a
+    if a < b:
+        continue
+    print(c)
+
+print('Задание 2')
+a=0
+b=100
+while a<=b:
+    print(a)
+    a += 2
+
+
+
+a = 1
+b = 300
+print('Задание 3')
+while a<b:
+    print(a)
+    a += 2
