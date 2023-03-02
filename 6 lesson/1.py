fe1566a36a886e5f3a123912fcb5a0a57708edba
Maksim-Maley <124808155+Maksim-Maley@users.noreply.github.com>
2023-03-02 21:12:21 +0300

    6 lesson

---
+++ 6 lesson/1.py
@@ -0,0 +1,11 @@
+a = int(input("число:"))
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
+    
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(i*j,end=" ")
+    print()
